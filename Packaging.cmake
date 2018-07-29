84174e3da47b831a9d65210a8881f3cf6c281545
Xav83 <x.jouvenot@gmail.com>
2018-07-29 19:07:32 +0200

    Make packaging automatically with only JUCE_VERSION list

--- Packaging.cmake
+++ Packaging.cmake
@@ -2,20 +2,21 @@ include(CPackComponent)
 
 set(CPACK_PACKAGE_NAME "juceCppCheck")
 
+function(installCppCheckResult juceVersion)
+    INSTALL(FILES ${CMAKE_BINARY_DIR}/juce${juceVersion}_output.txt
+        DESTINATION .
+        COMPONENT juce${juceVersion}_Component
+    )
+    list(APPEND CPACK_COMPONENTS_ALL juce${juceVersion}_Component)
+endfunction(installCppCheckResult)
+
 if (APPLE)
     set(CPACK_SYSTEM_NAME "Darwin")
     set(CPACK_GENERATOR "ZIP")
 
-    INSTALL(FILES ${CMAKE_BINARY_DIR}/juce5.3.2_output.txt
-            DESTINATION .
-            COMPONENT juce5.3.2_Component
-    )
-    INSTALL(FILES ${CMAKE_BINARY_DIR}/jucedevelop_output.txt
-            DESTINATION .
-            COMPONENT jucedevelop_Component
-    )
-
-    set(CPACK_COMPONENTS_ALL juce5.3.2_Component jucedevelop_Component)
+    foreach(juceVersion ${JUCE_VERSION})
+        installCppCheckResult(${juceVersion})
+    endforeach(juceVersion)
 endif(APPLE)
 
 include(CPack)
